dde5cc32ec9be080a8e35ca0cb58d32110d2e783
saske-pro <46398494+saske-pro@users.noreply.github.com>
2019-03-17 12:27:25 +0100

    Update bot.js

--- bot.js
+++ bot.js
@@ -1494,7 +1494,7 @@ msg.channel.send(embed).then(() => {
 
 client.on('ready', function(){	
     var ms = 40000 ;	
-    var setGame = ['Chicago Server'];	
+    var setGame = ['HL4E '];	
     var i = -1;	
     var j = 0;	
     setInterval(function (){	
